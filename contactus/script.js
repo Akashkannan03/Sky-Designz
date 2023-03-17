dcefdf5d42ee6cd04fe27448f850210f5e4c26b1
Akash <akashkannan@Akashs-MacBook-Air.local>
2023-03-17 16:27:58 +0530

    initial commit

---
+++ contactus/script.js
@@ -0,0 +1,7 @@
+ const form =document.getElementById('form');
+ const username =document.getElementById('username');
+ const email =document.getElementById('email');
+ const services =document.getElementById('services');
+ const yourmessage =document.getElementById('yourmessage');
+
+ form.addEventListener('')
